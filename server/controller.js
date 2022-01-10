efff73f5ba6b68ac4555e841e04d9d885ef4e710
Rae Carter <92331343+heimdalalr@users.noreply.github.com>
2022-01-10 09:24:47 -0500

    readibility changes and additional wireframes uploaded

--- server/controller.js
+++ server/controller.js
@@ -43,39 +43,3 @@ module.exports = {
 
 
 }
-
-
-
-
-
-
-
-
-//Friend's reccomendended shows
-// app.get("api/recommended", (req, res) => {
-
-//     const friendRecc = [
-//         "Show 1",
-//         "Show 2",
-//         "Show 3",
-//         "Show 4",
-//         "Show 5"
-//     ];
-
-//     //choose random friend req
-//     let randomIndex = Math.floor(Math.random() * friendRecc.length);
-//     let randomFriendRecc = friendRecc[randomIndex];
-
-//     res.status(200).send(randomFriendRecc);
-// });
-
-// personal list of shows in progress
-// app.get("api/progress", (req,res) => {
-//     const personalList = [
-//         "Squid Game - Season 1, Episode 8 of 9",
-//         "YOU - Season 3, Episode 7 of 10",
-//         "Tiger King - Season 1, Episode 2 of 5",
-//         "Narcos: Mexico - Season 1, Episode 1 of 10",
-//         "Maid - Season 1, Episode 9 of 10"
-//     ]
-// })
--- server/index.js
+++ server/index.js
@@ -1,30 +1,34 @@
+// imports express module which has methods for routing HTTP requests, cors allows a server to indicate origins from which a browser permits loading resources
 const express = require("express");
 const cors = require("cors")
 const controller = require('./controller.js')
-//let db = require('./db.json')
+
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+// main feature of quickfire reccomendations
 app.get(`/api/recommended`, (req, res) => {
   const recommendations = ["Lost in Space", "Jojo's Bizarre adventure", "Hellbound", "Arcane", "The Witcher"];
 
-  //choose random friend req
+  //choose random friend req by taking the function math.floor and multiplying by the length of the array of predetermined reccomendation
   let randomIndex = Math.floor(Math.random() * recommendations.length);
   let randomRecommendation = recommendations[randomIndex];
 
   res.status(200).send(randomRecommendation);
 });
+// sends the recommendation to the front end
 
+// series grouped by an object with key and value pairs that load on pageload of the website.
 let series = [
     {
-        "id": 1,
-        "seriesTitle": "Squid Game",
-        "season": 1,
-        "currentEpisode": 9,
-        "totalEpisodes": 10
+        "id": 1, //key id, 1 the value being the unique identifier for the tv show
+        "seriesTitle": "Squid Game", // key seriesTitle with the value being Squid Game
+        "season": 1, //key for current season
+        "currentEpisode": 9, //key for current episode 
+        "totalEpisodes": 10 // key for total episodes of current seasons
     },
     {
         "id": 2,
@@ -56,36 +60,27 @@ let series = [
     }
 ]
 
+// the get endpoint will send the series to the webstite upon the page load
 app.get(`/api/series`, (req, res) => {
     
     res.status(200).send(series);
 })
 
+// the post endput is the feature that allows the current episode to be incremented
 app.post(`/api/series/`, (req, res) => {
   console.log('in post backend')
     console.log(req.body)
   const newEpisodeValue = req.body.currentEpisode
   console.log("new episode value =>",newEpisodeValue)
-  for(let i=0; i < series.length; i++){
+  for(let i=0; i < series.length; i++){ 
       if(series[i].id === req.body.currentID){
           series[i].currentEpisode = newEpisodeValue
       }
   }
-//   let episode = series.filter(series => series.id === req.body.currentID)
-//   console.log("episode =>",episode)
-//   if (episode) {
-//     episode.currentEpisode = newEpisodeValue
-//   }
+
   console.log("serieslist =>", series)
   res.status(200).send(series)
 })
 
-// app.get('/',function(req,res) {
-//     res.sendFile(path.join(__dirname, 'client/index.html'));
-//   });
-
-//app.get(`/api/reccommended`, controller.friendRecc);
-//app.get(`api/progress`, controller.progress);
-//app.put(`api/update`, controller.update);
 const port = process.env.PORT || 7777
 app.listen(port, () => console.log(`Server live on ${port}`));
